790d2c3126aaf035fcb7a6f1817950fffc5be3a1
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-10-05 22:40:16 -0700

    Add todos

--- main.py
+++ main.py
@@ -1,50 +1,72 @@
-import datetime
-import io
 import sys
+from datetime import date
 
 import pandas as pd
-import requests
+# https://pypi.org/project/yfinance/
+import yfinance as yf
 
-# read path to the data file from arguments
-
-
-# Download Bank of Canada exchange rates
-def get_rates():
-    start_date = datetime.date.today().replace(day=1, month=1).isoformat()
-    end_date = datetime.date.today().isoformat()
-    path = f"https://www.bankofcanada.ca/valet/observations/FXUSDCAD/csv?start_date={start_date}&end_date={end_date}"
-
-    csv = requests.get(path).content.decode("utf-8")
-    csv = csv[csv.find('"date"'):]
-    return pd.read_csv(io.StringIO(csv))
-
-
-rates_df = get_rates()
-rates_df['date'] = pd.to_datetime(rates_df['date'], format='%Y-%m-%d')
+from bank_of_canada import get_cadx_rates
 
 if len(sys.argv) < 2:
     print("Please provide the path to the data file")
     exit(1)
 
-df = pd.read_excel(sys.argv[1])
-df.head()
-# Index(['Transaction Date', 'Settlement Date', 'Action', 'Symbol',
-# 'Description', 'Quantity', 'Price', 'Gross Amount', 'Commission',
-# 'Net Amount', 'Currency', 'Account #', 'Activity Type', 'Account Type'],
-# dtype='object')
 
-df['Settlement Date'] = pd.to_datetime(df['Settlement Date'], format='%Y-%m-%d')
+df = pd.read_excel(sys.argv[1])
+df["Settlement Date"] = pd.to_datetime(df["Settlement Date"]).dt.date
+df["Transaction Date"] = pd.to_datetime(df["Transaction Date"]).dt.date
+
+
+start_date = df["Settlement Date"].min().isoformat()
+end_date = date.today().isoformat()
+
+stocks = [s for s in df["Symbol"].unique() if not any(c.isdigit() for c in s)]
+tsx_stocks = [s for s in stocks if s.endswith(".TO")]
+# Remove TSX stocks without .TO suffix
+stocks = list(set(stocks) - set([s.removesuffix(".TO") for s in tsx_stocks]))
+# Replace TSX stocks without .TO suffix with .TO suffix
+df["Symbol"] = df["Symbol"].replace(
+    [s.removesuffix(".TO") for s in tsx_stocks], tsx_stocks
+)
+
+tsx_filter = df["Symbol"].map(lambda x: x + ".TO").isin(tsx_stocks)
+df.loc[tsx_filter, "Symbol"] = df[tsx_filter]["Symbol"].map(lambda x: x + ".TO")
+
+rates_df = get_cadx_rates(start_date, end_date)
 
 trades_df = df[df["Activity Type"] == "Trades"]
 dividents_df = df[df["Activity Type"] == "Dividends"]
 
-
 # Merge on closest earlier or same date
-trades_df = pd.merge_asof(trades_df.sort_values('Settlement Date'), rates_df, left_on='Settlement Date', right_on='date', direction='backward')
-trades_df.loc[trades_df['Currency'] == 'USD', 'Price'] *= trades_df['FXUSDCAD']
+trades_df = pd.merge_asof(
+    trades_df.sort_values("Settlement Date"),
+    rates_df,
+    left_on="Settlement Date",
+    right_on="date",
+    direction="backward",
+)
+trades_df["PriceInAccountCurrency"] = trades_df["Price"]
+trades_df.loc[trades_df["Currency"] == "USD", "PriceInCAD"] = (
+    trades_df["Price"] * trades_df["FXUSDCAD"]
+)
+del trades_df["Price"]
 
-dividents_df = pd.merge_asof(dividents_df.sort_values('Settlement Date'), rates_df, left_on='Settlement Date', right_on='date', direction='backward')
-dividents_df.loc[dividents_df['Currency'] == 'USD', 'Price'] *= dividents_df['FXUSDCAD']
+dividents_df = pd.merge_asof(
+    dividents_df.sort_values("Settlement Date"),
+    rates_df,
+    left_on="Settlement Date",
+    right_on="date",
+    direction="backward",
+)
+dividents_df["PriceInAccountCurrency"] = dividents_df["Price"]
+dividents_df.loc[dividents_df["Currency"] == "USD", "PriceInCAD"] = (
+    dividents_df["Price"] * dividents_df["FXUSDCAD"]
+)
+del dividents_df["Price"]
 
 
-(trades_df["Price"] * trades_df["Quantity"]).sum()
+# TODO: contributions and withdrawals
+# TODO: current portfolio value
+# TODO: dividends and distributions
+# TODO: fees and expenses
+# TODO: interest
