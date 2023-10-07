2030aedf455ba0e890e66d46d426e014d2329182
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-10-06 21:19:21 -0700

    Simplify getting exchange rates

--- main.py
+++ main.py
@@ -17,23 +17,35 @@ df = pd.read_excel(sys.argv[1])
 df["Settlement Date"] = pd.to_datetime(df["Settlement Date"])
 df["Transaction Date"] = pd.to_datetime(df["Transaction Date"])
 
-
 start_date = df["Settlement Date"].min().isoformat()
 end_date = date.today().isoformat()
 
+rates_df = get_cadx_rates(start_date, end_date)
+# Merge rates with transactions
+df = pd.merge_asof(
+    df.sort_values("Settlement Date"),
+    rates_df,
+    left_on="Settlement Date",
+    right_on="date",
+    direction="backward",
+)
+df["PriceInAccountCurrency"] = df["Price"]
+df.loc[df["Currency"] == "USD", "PriceInCAD"] = (
+    df["Price"] * df["FXUSDCAD"]
+)
+del df["Price"]
+
 stocks = [s for s in df["Symbol"].dropna().unique() if not any(c.isdigit() for c in s)]
+# Securities listed on TSX often have .TO suffix
 tsx_stocks = [s for s in stocks if s.endswith(".TO")]
-# Remove TSX stocks without .TO suffix
+# Find TSX listed stocks without .TO suffix
 stocks = list(set(stocks) - set([s.removesuffix(".TO") for s in tsx_stocks]))
-# Replace TSX stocks without .TO suffix with .TO suffix
+# Replace stocks without .TO suffix with stocks with .TO suffix
 df["Symbol"] = df["Symbol"].replace(
     [s.removesuffix(".TO") for s in tsx_stocks], tsx_stocks
 )
-
 tsx_filter = df["Symbol"].map(lambda x: str(x) + ".TO").isin(tsx_stocks)
 df.loc[tsx_filter, "Symbol"] = df[tsx_filter]["Symbol"].map(lambda x: x + ".TO")
-
-rates_df = get_cadx_rates(start_date, end_date)
 
 deposits_df = df[df["Activity Type"] == "Deposits"]
 withdrawals_df = df[df["Activity Type"] == "Withdrawals"]
@@ -42,34 +54,9 @@ interest_df = df[df["Activity Type"] == "Interest"]
 trades_df = df[df["Activity Type"] == "Trades"]
 dividents_df = df[df["Activity Type"] == "Dividends"]
 
-# Merge on closest earlier or same date
-trades_df = pd.merge_asof(
-    trades_df.sort_values("Settlement Date"),
-    rates_df,
-    left_on="Settlement Date",
-    right_on="date",
-    direction="backward",
-)
-trades_df["PriceInAccountCurrency"] = trades_df["Price"]
-trades_df.loc[trades_df["Currency"] == "USD", "PriceInCAD"] = (
-    trades_df["Price"] * trades_df["FXUSDCAD"]
-)
-del trades_df["Price"]
+import pdb
 
-dividents_df = pd.merge_asof(
-    dividents_df.sort_values("Settlement Date"),
-    rates_df,
-    left_on="Settlement Date",
-    right_on="date",
-    direction="backward",
-)
-dividents_df["PriceInAccountCurrency"] = dividents_df["Price"]
-dividents_df.loc[dividents_df["Currency"] == "USD", "PriceInCAD"] = (
-    dividents_df["Price"] * dividents_df["FXUSDCAD"]
-)
-del dividents_df["Price"]
-
-import pdb; pdb.set_trace()
+pdb.set_trace()
 
 
 # TODO: contributions and withdrawals
